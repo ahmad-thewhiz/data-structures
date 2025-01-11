3a8cf486f602f0d2132ca2d6c8d398e2eafad00b
Ahmad Raza <114557897+ahmad-thewhiz@users.noreply.github.com>
2025-01-11 22:05:56 +0530

    Update min_cost_climbing_stairs.cpp

--- Dynamic Programming/min_cost_climbing_stairs.cpp
+++ Dynamic Programming/min_cost_climbing_stairs.cpp
@@ -47,3 +47,33 @@ public:
         return minCost(cost, n);
     }
 };
+
+// bottom-up (optimized for space complexity)
+class Solution {
+private:
+    int minCost(vector<int>& cost, int n) {
+        if(n==0)
+            return cost[0];
+        if(n==1)
+            return min(cost[0], cost[1]);
+        
+        int prev1=cost[0];
+        int prev2=cost[1];
+
+        for (int i = 2; i < n; i++) {  
+            int curr = cost[i] + min(prev1, prev2);
+            prev1=prev2;
+            prev2=curr;
+        }
+
+        return min(prev1, prev2);
+    }
+
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+
+        return minCost(cost, n);
+    }
+};
+
