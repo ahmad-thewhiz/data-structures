3135d8c97fce99b95aa5919b94ec4e8781bfbff9
Ahmad Raza <114557897+ahmad-thewhiz@users.noreply.github.com>
2025-01-12 23:51:06 +0530

    added solution with best complexity

--- dynamic_prog/max_sum_non_adjacent_elements.cpp
+++ dynamic_prog/max_sum_non_adjacent_elements.cpp
@@ -56,6 +56,28 @@ int sovleTab(vector<int>&nums) {
     return dp[n-1];
 }
 
+int solveBest(vector<int>& nums) {
+    int n = nums.size();
+
+    if (n == 0) return 0;
+    if (n == 1) return nums[0];
+
+    int prev2 = nums[0];                    // Maximum sum up to the 1st element
+    int prev1 = max(nums[0], nums[1]);      // Maximum sum up to the 2nd element
+
+    for (int i = 2; i < n; i++) {
+        int incl = prev2 + nums[i];         // Include the current element
+        int excl = prev1;                   // Exclude the current element
+        int curr = max(incl, excl);         // Max sum at index i
+
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+}
+
+
 int maximumNonAdjacentSum(vector<int> &nums){
     int n=nums.size();
 
